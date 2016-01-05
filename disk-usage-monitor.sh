cb5350511f7b95801301282cd45298a8056269cf
Marcus Vinicius de Mattos Ramos <mv.mramos94@gmai.com>
2016-01-05 16:15:34 -0200

    Improvement in script: disk-usage-monitor.sh

--- disk-usage-monitor.sh
+++ disk-usage-monitor.sh
@@ -4,7 +4,12 @@
 LIMITE="80"
 EMAIL="your@email.com"
 
+for cmd in mutt df awk; do
+      type $cmd &> /dev/null || { echo "THIS SCRIPT NEEDS THE PKG: $cmd TO WORK PROPERLY"; exit 1;  }
+  done
+
 for $PART in $(df -h | awk '{print $1}'); do
+
     USAGE=(df -h $PART | awk '{print $5}' | grep -v Use | sort -nr | awk -F % '{print $1}' | head -n1)
 
     if [ $USAGE -gt "$LIMITE" ]
