820e7eccbfa0d6987cf67a3d723c29ca8da256af
Marcus Vinicius de Mattos Ramos <mv.mramos94@gmail.com>
2016-01-05 19:54:03 +0000

    Fix some bugs in disk-usage-monitor.sh

--- disk-usage-monitor.sh
+++ disk-usage-monitor.sh
@@ -8,12 +8,12 @@ for cmd in mutt df awk; do
       type $cmd &> /dev/null || { echo "THIS SCRIPT NEEDS THE PKG: $cmd TO WORK PROPERLY"; exit 1;  }
   done
 
-for $PART in $(df -h | awk '{print $1}'); do
+for PART in $(df -h | grep -v Use | awk '{print $6}'); do
 
-    USAGE=(df -h $PART | awk '{print $5}' | grep -v Use | sort -nr | awk -F % '{print $1}' | head -n1)
+    USAGE=$(df -h $PART | awk '{print $5}' | grep -v Use | sort -nr | awk -F % '{print $1}' | head -n1)
 
-    if [ $USAGE -gt "$LIMITE" ]
+    if [[ $USAGE -gt "$LIMITE" ]]
     then
-        echo "THE DISK USAGE IT IS "$USAGE"%, PLEASE CHECK THE FILES IN THE PARTITION: $PART UNTIL THE USAGE REACH THE LIMIT." | mutt -s "USAGE WARNING" $EMAIL
+        echo "THE DISK USAGE IT IS "$USAGE"%, PLEASE CHECK THE FILES IN THE PARTITION: "$PART" UNTIL THE USAGE REACH THE LIMIT." | mutt -s "USAGE WARNING" $EMAIL
     fi
 done
