65a2fd42e54aa5a9122c54d963b92e7ad6b5addb
Marcus Vinicius de Mattos Ramos <mv.mramos94@gmai.com>
2016-01-06 10:33:35 -0200

    Edited var name from: disk-usage-monitor.sh

--- disk-usage-monitor.sh
+++ disk-usage-monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Marcus Vinícius de Mattos Ramos
 # VARS:
-LIMITE="80"
+LIMIT="80"
 EMAIL="your@email.com"
 
 for cmd in mutt df awk; do
@@ -12,7 +12,7 @@ for PART in $(df -h | grep -v Use | awk '{print $6}'); do
 
     USAGE=$(df -h $PART | awk '{print $5}' | grep -v Use | sort -nr | awk -F % '{print $1}' | head -n1)
 
-    if [[ $USAGE -gt "$LIMITE" ]]
+    if [[ $USAGE -gt "$LIMIT" ]]
     then
         echo "THE DISK USAGE IT IS "$USAGE"%, PLEASE CHECK THE FILES IN THE PARTITION: "$PART" UNTIL THE USAGE REACH THE LIMIT." | mutt -s "USAGE WARNING" $EMAIL
     fi
